3c8d11cf95f8440c730e600a944b8d128fb08843
MuhammadAizazA <aizaz.ali.shah.0205@gamil.com>
2023-11-24 12:36:10 +0500

    made comments

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
-from subprocess import run,CalledProcessError,PIPE
+from subprocess import run, CalledProcessError, PIPE
 import shlex
-from sys import executable,exit
+from sys import executable, exit
 
     
+# Check if a given tool is installed by running its '--version' command.
 def is_tool_installed(tool):
     try:
         run([tool, "--version"], stdout=PIPE, stderr=PIPE, check=True)
@@ -10,6 +11,8 @@ def is_tool_installed(tool):
     except CalledProcessError:
         return False
 
+
+# Install 'pipx' if it is not already installed.
 def install_pipx():
     try:
         if is_tool_installed("pipx"):
@@ -21,6 +24,8 @@ def install_pipx():
         print(f"Error installing pipx: {e}")
         exit(1)
 
+
+# Install 'poetry' if it is not already installed.
 def install_poetry():
     try:
         if is_tool_installed("poetry"):
@@ -32,21 +37,27 @@ def install_poetry():
         print(f"Error installing poetry: {e}")
         exit(1)
         
+
+# Set up the package using 'poetry'.
 def setup_package():
     try:
-        #this will check if system is having 
+        # Check the version of poetry
         run(shlex.split("poetry -V"))
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        #this commands will setup package
+        # Commands to set up the package
         run(shlex.split("poetry install"))
         run(shlex.split("poetry update"))
         run(shlex.split("poetry shell"))
+
+
+# Main function to execute the installation and setup process.
 def main():
     install_pipx()
     install_poetry()
     setup_package()
+
 
 if __name__ == "__main__":
     main()
